c74df0def24abbece8af6415c4a77a7216b1bf25
zaid-khatri <76276783+zaid-khatri@users.noreply.github.com>
2021-06-26 11:24:35 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,7 @@
+function back(){
+    window.location="activity_1.html"
+}
+function getScore(){
+    score= localStorage.getItem("score")
+    document.getElementById("update").innerHTML="<h1>Score:" +score +"</h1>";
+}
